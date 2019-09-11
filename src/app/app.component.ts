317a4508f6f749dcfc31f35e2b1271ccd48812f6
azar103 <azzarroukanis@gmail.com>
2019-09-11 12:12:51 +0100

    firebase configured

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
-
+import * as firebase from 'firebase';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'BooksApp';
+  constructor() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyCaElaBz3zU0kLor-Lg67xOAGAiXftbawA",
+      authDomain: "booksapp-d9ba4.firebaseapp.com",
+      databaseURL: "https://booksapp-d9ba4.firebaseio.com",
+      projectId: "booksapp-d9ba4",
+      storageBucket: "",
+      messagingSenderId: "291217776014",
+      appId: "1:291217776014:web:d4bf026e8d46bb151d98df"
+    };
+    firebase.initializeApp(firebaseConfig);
+
+  }
 }
